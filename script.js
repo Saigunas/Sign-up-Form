43c31f071f29d0e5c186f2f5979c33acaa722ad6
Saigunas <saigunasb777@gmail.com>
2022-03-24 19:37:44 +0200

    added password validation

---
+++ script.js
@@ -0,0 +1,21 @@
+const passwordInput = document.getElementById('pass');
+const passwordConfirmInput = document.getElementById('pass-confirm');
+
+passwordConfirmInput.addEventListener('change', comparePasswords);
+passwordInput.addEventListener('change', comparePasswords);
+
+function comparePasswords() {
+    if(passwordInput.value !== passwordConfirmInput.value) {
+        passwordInput.classList.add('invalid');
+        passwordConfirmInput.classList.add('invalid');
+        passwordInput.classList.remove('valid');
+        passwordConfirmInput.classList.remove('valid');
+        passwordInput.setCustomValidity("Password must match");
+        return;
+    }     
+    passwordInput.setCustomValidity("");
+    passwordInput.classList.remove('invalid');
+    passwordConfirmInput.classList.remove('invalid');
+    passwordInput.classList.add('valid');
+    passwordConfirmInput.classList.add('valid');
+}
